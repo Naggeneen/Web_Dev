b0c054ada9bb12a7f4b2138d686db571e9f4eb60
Alen_Robinson <98682179+Naggeneen@users.noreply.github.com>
2023-02-15 22:54:31 +0600

    Add files via upload

---
+++ Lab3/Javascript/section_2/2_13_output_prime_numbers.js
@@ -0,0 +1,7 @@
+let n;
+n= prompt("Choose number:",'');
+for(let i=0; i<=n;i++){
+    if(i%2==0){
+        alert(i);
+    }
+}
---
+++ Lab3/Javascript/section_2/2_13_repeat_until_input_is_incorrect.js
@@ -0,0 +1,4 @@
+let num = 1;
+while(num <=100 && num){
+    num = prompt("Number:",'');
+}
